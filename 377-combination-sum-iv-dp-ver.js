b25f6250688385e5afcd6b2193364d3b091e638e
dan kim <guynameddan@gmail.com>
2021-12-28 01:17:40 -0700

    Add prob 377 solution

---
+++ 377-combination-sum-iv-dp-ver.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/combination-sum-iv/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var combinationSum4 = function(nums, target) {
+    let memo = new Map();
+    
+    memo.set(0, 1);
+        
+    for (let i = 1; i <= target; i++) {
+        memo.set(i, 0);
+        
+        for (let num of nums) {
+            if (i - num >= 0) {
+                memo.set(i, memo.get(i) + memo.get(i - num));
+            }
+        }
+    }
+        
+    return memo.get(target);
+}
