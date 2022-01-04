4b8ab657beaf2e6be1fc06e4026aa6ddc76825b5
dan kim <guynameddan@gmail.com>
2022-01-03 17:59:11 -0700

    Add prob 100 solution

---
+++ 100-same-tree.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/same-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+ var isSameTree = function(p, q) {
+    
+    function dfs(p, q) {
+        if (!p && !q) return true
+        
+        if (!p || !q || p.val !== q.val) return false;
+        
+        return (dfs(p.left, q.left) && dfs(p.right, q.right));
+    }
+    
+    return dfs(p, q);
+    
+};
