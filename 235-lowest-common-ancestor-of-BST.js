de1f78f2db3ba8eda96e5e1a8520f0d7d9a6bae6
dan kim <guynameddan@gmail.com>
2022-01-05 16:56:29 -0700

    Add prob 235 solution

---
+++ 235-lowest-common-ancestor-of-BST.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+ var lowestCommonAncestor = function(root, p, q) {
+    let currentNode = root;
+    
+    while (currentNode != null) {
+        if (p.val > currentNode.val && q.val > currentNode.val) {
+            currentNode = currentNode.right;
+        } else if (p.val < currentNode.val && q.val < currentNode.val) {
+            currentNode = currentNode.left;
+        } else {
+            return currentNode;
+        }
+    }
+};
