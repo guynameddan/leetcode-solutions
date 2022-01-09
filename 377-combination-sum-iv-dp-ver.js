7e2be983ee29488d168be6a65eca27c6cf3c36a0
dan kim <guynameddan@gmail.com>
2022-01-09 15:57:16 -0700

    Update prob 377 solution

--- 377-combination-sum-iv-dp-ver.js
+++ 377-combination-sum-iv-dp-ver.js
@@ -21,3 +21,20 @@
         
     return memo.get(target);
 }
+
+// or with an array
+var combinationSum4 = function(nums, target) {
+    let memo = new Array(target + 1).fill(0);
+    
+    memo[0] = 1;
+        
+    for (let i = 1; i <= target; i++) {        
+        for (let num of nums) {
+            if (i - num >= 0) {
+                memo[i] = memo[i] + memo[i-num];
+            }
+        }
+    }
+        
+    return memo[target];
+}
