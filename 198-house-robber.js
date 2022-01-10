84a5aadecada28203fa219f1f95eb9178dac1850
dan kim <guynameddan@gmail.com>
2022-01-09 20:59:01 -0700

    Add prob 198 solution

---
+++ 198-house-robber.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/house-robber/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var rob = function(nums) {
+    let dp = new Array(nums.length);
+    
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    
+    // think of this as "what has more? robbing the
+    // current house and the max of robbing any houses
+    // from 2 houses ago or robbing only the max of the
+    // houses up to the previous house?"
+    for (let i = 2; i < dp.length; i++) {
+        dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+    }
+    
+    return dp[nums.length-1];
+};
