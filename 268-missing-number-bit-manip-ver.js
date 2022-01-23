ce03013cf31c4499ec7fa62ff6fc8b2f3a27cb49
dan kim <guynameddan@gmail.com>
2022-01-23 10:55:46 -0700

    Add prob 268 bit manipulation comments

--- 268-missing-number-bit-manip-ver.js
+++ 268-missing-number-bit-manip-ver.js
@@ -2,6 +2,10 @@
 /**
  * @param {number[]} nums
  * @return {number}
+
+a ^ a = 0
+a ^ b ^ b = a
+the missing number will not have a pair to cancel it out
  */
  var missingNumber = function(nums) {
     let result = nums.length;
