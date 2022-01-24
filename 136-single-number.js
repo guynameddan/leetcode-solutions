3bb239c79454477b387bb760339c82ff191e2aaf
dan kim <guynameddan@gmail.com>
2022-01-24 01:13:38 -0700

    Add prob 136 solution

---
+++ 136-single-number.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/missing-number/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 
+ * a ^ a = 0
+ * a ^ b ^ b = a
+ * the missing number will not have a pair to cancel it out
+ */
+ var missingNumber = function(nums) {
+    let result = nums.length;
+    
+    for (let i = 0; i < nums.length; i++) {
+        result ^= i ^ nums[i];
+    }
+    
+    return result;
+};
