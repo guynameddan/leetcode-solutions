724c7264444b452fefb40ca0ed36cd2faf86b706
dan kim <guynameddan@gmail.com>
2022-02-04 13:41:28 -0700

    Add min length substring initial solution

---
+++ meta-facebook-prep/min-length-substrings.js
@@ -0,0 +1,90 @@
+/**
+ * 
+Minimum Length Substrings
+You are given two strings s and t. You can select any substring of string s and rearrange the characters of the selected substring. Determine the minimum length of the substring of s such that string t is a substring of the selected substring.
+Signature
+int minLengthSubstring(String s, String t)
+Input
+s and t are non-empty strings that contain less than 1,000,000 characters each
+Output
+Return the minimum length of the substring of s. If it is not possible, return -1
+Example
+s = "dcbefebce"
+t = "fd"
+output = 5
+Explanation:
+Substring "dcbef" can be rearranged to "cfdeb", "cefdb", and so on. String t is a substring of "cfdeb". Thus, the minimum length required is 5.
+ */
+function minLengthSubstring(s, t) {
+    // console.log(t);
+
+    let t_letters = new Map(),
+        left = 0,
+        right = 0,
+        count = 0,
+        tChars = t.length,
+        min = Infinity,
+        temp = 0;
+    
+    for (let letter of t) {
+      if (!t_letters.has(letter)) t_letters.set(letter, 1);
+      else {
+        count = t_letters.get(letter);
+        t_letters.set(letter, ++count);
+      }
+    }
+
+    // console.log(t_letters);
+    // console.log(count);
+    
+    while (right != s.length) {
+    //   console.log("right: " + right);
+    //   console.log(tChars);
+    //   console.log(s[right]);
+    //   console.log(t_letters.has(s[right]));
+
+      count = t_letters.get(s[right]);
+
+    //   console.log(count);
+        
+      if (t_letters.has(s[right++]) && count > 0) {
+        // right++;
+        // console.log("I'm in");
+        // console.log(right);
+        t_letters.set(s[right], --count)
+        tChars--;
+      }
+
+    //   console.log(tChars);
+      
+      while (tChars === 0) {
+        if (t_letters.has(s[left++])) {
+          tChars++;
+          temp = right - left + 1;
+          min = Math.min(temp, min);
+        }
+      }
+      
+    }
+    
+    return min === Infinity ? -1 : min;
+    
+  }
+
+  console.log(minLengthSubstring("dcbefebce", "fd"));
+
+  let a = "bfbeadbcbcbfeaaeefcddcccbbbfaaafdbebedddf",
+    b = "cbccfafebccdccebdd";
+
+/**
+ * a 1
+ * b 3
+ * c 7
+ * d 3
+ * e 2
+ * f 2
+ */
+
+
+  console.log(minLengthSubstring(a, b));
+
