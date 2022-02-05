4c2ee0f76def8a62bf25966bc49e23f78afb50e3
dan kim <guynameddan@gmail.com>
2022-02-05 00:40:51 -0700

    Clean up min length substring initial solution

--- meta-facebook-prep/min-length-substrings.js
+++ meta-facebook-prep/min-length-substrings.js
@@ -16,60 +16,43 @@ Explanation:
 Substring "dcbef" can be rearranged to "cfdeb", "cefdb", and so on. String t is a substring of "cfdeb". Thus, the minimum length required is 5.
  */
 function minLengthSubstring(s, t) {
-    // console.log(t);
-
-    let t_letters = new Map(),
-        left = 0,
-        right = 0,
-        count = 0,
-        tChars = t.length,
-        min = Infinity,
-        temp = 0;
-    
-    for (let letter of t) {
-      if (!t_letters.has(letter)) t_letters.set(letter, 1);
-      else {
-        count = t_letters.get(letter);
-        t_letters.set(letter, ++count);
-      }
+  let t_letters = new Map(),
+      left = 0,
+      right = 0,
+      count = 0,
+      tChars = t.length,
+      min = Infinity,
+      temp = 0;
+  
+  for (let letter of t) {
+    if (!t_letters.has(letter)) t_letters.set(letter, 1);
+    else {
+      count = t_letters.get(letter);
+      t_letters.set(letter, ++count);
     }
-
-    // console.log(t_letters);
-    // console.log(count);
-    
-    while (right != s.length) {
-    //   console.log("right: " + right);
-    //   console.log(tChars);
-    //   console.log(s[right]);
-    //   console.log(t_letters.has(s[right]));
-
-      count = t_letters.get(s[right]);
-
-    //   console.log(count);
-        
-      if (t_letters.has(s[right++]) && count > 0) {
-        // right++;
-        // console.log("I'm in");
-        // console.log(right);
-        t_letters.set(s[right], --count)
-        tChars--;
-      }
-
-    //   console.log(tChars);
+  }
+  
+  while (right != s.length) {
+    count = t_letters.get(s[right]);
       
-      while (tChars === 0) {
-        if (t_letters.has(s[left++])) {
-          tChars++;
-          temp = right - left + 1;
-          min = Math.min(temp, min);
-        }
-      }
-      
+    if (t_letters.has(s[right++]) && count > 0) {
+      t_letters.set(s[right], --count)
+      tChars--;
     }
     
-    return min === Infinity ? -1 : min;
+    while (tChars === 0) {
+      if (t_letters.has(s[left++])) {
+        tChars++;
+        temp = right - left + 1;
+        min = Math.min(temp, min);
+      }
+    }
     
   }
+  
+  return min === Infinity ? -1 : min;
+  
+}
 
   console.log(minLengthSubstring("dcbefebce", "fd"));
 
