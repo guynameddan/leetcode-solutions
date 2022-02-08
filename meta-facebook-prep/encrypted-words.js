188c4dc85d6f637fd3ee4c3609fe12e456d807ea
dan kim <guynameddan@gmail.com>
2022-02-07 17:02:30 -0700

    Clean up encrypted words file

--- meta-facebook-prep/encrypted-words.js
+++ meta-facebook-prep/encrypted-words.js
@@ -1,57 +1,59 @@
-/**
- * Encrypted Words
-You've devised a simple encryption method for alphabetic strings that shuffles the characters in such a way that the resulting string is hard to quickly read, but is easy to convert back into the original string.
-When you encrypt a string S, you start with an initially-empty resulting string R and append characters to it as follows:
-Append the middle character of S (if S has even length, then we define the middle character as the left-most of the two central characters)
-Append the encrypted version of the substring of S that's to the left of the middle character (if non-empty)
-Append the encrypted version of the substring of S that's to the right of the middle character (if non-empty)
-For example, to encrypt the string "abc", we first take "b", and then append the encrypted version of "a" (which is just "a") and the encrypted version of "c" (which is just "c") to get "bac".
-If we encrypt "abcxcba" we'll get "xbacbca". That is, we take "x" and then append the encrypted version "abc" and then append the encrypted version of "cba".
-Input
-S contains only lower-case alphabetic characters
-1 <= |S| <= 10,000
-Output
-Return string R, the encrypted version of S.
-Example 1
-S = "abc"
-R = "bac"
-Example 2
-S = "abcd"
-R = "bacd"
-Example 3
-S = "abcxcba"
-R = "xbacbca"
-Example 4
-S = "facebook"
-R = "eafcobok"
- */
 function findEncryptedWord(s) {
-    if (!s) return;
-    if (s.length === 1) return s;
-    
-    let mid = 0,
-        result = "";
-    
-    if (s.length % 2 === 0) {
-      mid = (s.length / 2) - 1;
-    } else {
-      mid = Math.floor(s.length / 2);
-    }
-
-    result += s[mid] + findEncryptedWord(s.substring(0,mid)) + findEncryptedWord(s.substring(mid+1, s.length));
-
-    return result;
+  if (!s) return "";
+  if (s.length === 1) return s;
+  
+  let mid = 0,
+      result = "";
+  
+  if (s.length % 2 === 0) {
+    mid = (s.length / 2) - 1;
+  } else {
+    mid = Math.floor(s.length / 2);
   }
+  
+  let left = findEncryptedWord(s.substring(0,mid)),
+      right = findEncryptedWord(s.substring(mid+1, s.length));
+  
+  result += s[mid] + left + right;
+  
+  return result;
+}
 
-  let s = "abc",
-    expected_1 = "bac";
-    output_1 = findEncryptedWord(s);
 
-    console.log(output_1);
+// These are the tests we use to determine if the solution is correct.
+// You can add your own at the bottom.
+function printString(str) {
+  var out = '["' + str + '"]';
+  return out;
+}
 
-  let s2 = "abcxcba",
-    R = "xbacbca";
+var test_case_number = 1;
 
-  output2 = findEncryptedWord(s2);
+function check(expected, output) {
+  var result = (expected == output);
+  var rightTick = "\u2713";
+  var wrongTick = "\u2717";
+  if (result) {
+    var out = rightTick + ' Test #' + test_case_number;
+    console.log(out);
+  }
+  else {
+    var out = '';
+    out += wrongTick + ' Test #' + test_case_number + ': Expected ';
+    out += printString(expected);
+    out += ' Your output: ';
+    out += printString(output);
+    console.log(out);
+  }
+  test_case_number++;
+}
 
-  console.log(output2);
+var s_1 = "abc";
+var expected_1 = "bac";
+var output_1 = findEncryptedWord(s_1);
+check(expected_1, output_1);
+
+var s_2 = "abcd";
+var expected_2 = "bacd";
+var output_2 = findEncryptedWord(s_2);
+check(expected_2, output_2);
