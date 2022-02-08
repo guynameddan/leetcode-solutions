a0abf3c7e02948467ef3d59f11c2e828d9ca18c2
dan kim <guynameddan@gmail.com>
2022-02-07 17:04:25 -0700

    Clean up change in a foreign currency file

--- meta-facebook-prep/change-in-a-foreign-currency.js
+++ meta-facebook-prep/change-in-a-foreign-currency.js
@@ -1,55 +1,67 @@
-/**
- * Change in a Foreign Currency
-You likely know that different currencies have coins and bills of different denominations. In some currencies, it's actually impossible to receive change for a given amount of money. For example, Canada has given up the 1-cent penny. If you're owed 94 cents in Canada, a shopkeeper will graciously supply you with 95 cents instead since there exists a 5-cent coin.
-Given a list of the available denominations, determine if it's possible to receive exact change for an amount of money targetMoney. Both the denominations and target amount will be given in generic units of that currency.
-Signature
-boolean canGetExactChange(int targetMoney, int[] denominations)
-Input
-1 ≤ |denominations| ≤ 100
-1 ≤ denominations[i] ≤ 10,000
-1 ≤ targetMoney ≤ 1,000,000
-Output
-Return true if it's possible to receive exactly targetMoney given the available denominations, and false if not.
-Example 1
-denominations = [5, 10, 25, 100, 200]
-targetMoney = 94
-output = false
-Every denomination is a multiple of 5, so you can't receive exactly 94 units of money in this currency.
-Example 2
-denominations = [4, 17, 29]
-targetMoney = 75
-output = true
-You can make 75 units with the denominations [17, 29, 29].
- */
+// Add any extra import statements you may need here
+
+
+// Add any helper functions you may need here
+
+
 function canGetExactChange(targetMoney, denominations) {
-    let result = false;
-    
-    if (targetMoney === 0) return true;
+  let result = false;
+
+  if (targetMoney === 0) return true;
+  if (targetMoney < 0) return false;
+
+
+  for (let i = denominations.length - 1; i >= 0 ; i--) {
+    if (targetMoney === 0 || result === true) return true;
     if (targetMoney < 0) return false;
 
-    
-    for (let i = denominations.length - 1; i >= 0 ; i--) {
-      if (targetMoney === 0 || result === true) return true;
-      if (targetMoney < 0) return false;
+    targetMoney -= denominations[i];
 
-      targetMoney -= denominations[i];
-      
-      result = canGetExactChange(targetMoney, denominations);
-    }
-    
-    return result;
+    result = canGetExactChange(targetMoney, denominations);
+  }
+
+  return result;
+}
+
+
+// These are the tests we use to determine if the solution is correct.
+// You can add your own at the bottom.
+function printString(str) {
+  var out = '["' + str + '"]';
+  return out;
+}
+
+var test_case_number = 1;
+
+function check(expected, output) {
+  var result = (expected == output);
+  var rightTick = "\u2713";
+  var wrongTick = "\u2717";
+  if (result) {
+    var out = rightTick + ' Test #' + test_case_number;
+    console.log(out);
+  }
+  else {
+    var out = '';
+    out += wrongTick + ' Test #' + test_case_number + ': Expected ';
+    out += printString(expected);
+    out += ' Your output: ';
+    out += printString(output);
+    console.log(out);
+  }
+  test_case_number++;
 }
 
 var target_1 = 94;
 var arr_1 = [5, 10, 25, 100, 200];
 var expected_1 = false;
 var output_1 = canGetExactChange(target_1, arr_1);
-
-console.log(output_1);
+check(expected_1, output_1);
 
 var target_2 = 75;
 var arr_2 = [4, 17, 29];
 var expected_2 = true;
 var output_2 = canGetExactChange(target_2, arr_2);
+check(expected_2, output_2);
 
-console.log(output_2);
+// Add your own test cases here
